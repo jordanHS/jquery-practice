50b6bd2593a96c4b0edf9a4515a2cea26f6e5cb6
Student <example@example.com>
2019-08-13 13:04:32 -0700

    create page with simple effects

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#lorem-ipsum").click(function() {
+});
+$(document).ready(function() {
+  $("h2").click(function()
+  $("#using-loremipsum").click(function() {
+    });
+  });
